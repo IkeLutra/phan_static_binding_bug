f79c561dd862be36229c8de5e46c9ee8c565b144
Will Otterburn <will.otterburn@themarketinglab.co.uk>
2019-03-14 17:35:32 +0000

    Add comments

--- static_binding_phan.php
+++ static_binding_phan.php
@@ -5,7 +5,9 @@ require_once __DIR__ . '/vendor/autoload.php';
 var_dump(get_class(A::create()));
 $b = B::create();
 var_dump(get_class($b));
+// This will trigger "Call to undeclared method \A::doSomething"
 $b->doSomething();
 $c = C::create();
 var_dump(get_class($c));
+// This will not as the docBloc is rewritten
 $c->doSomething();
